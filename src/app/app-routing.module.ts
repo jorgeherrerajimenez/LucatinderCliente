af05cdef36f484dd5a607193ecfbf0c00ff4e9b7
Marco Rueda <marcorueda92z@gmail.com>
2020-06-11 14:01:34 +0200

    configuración componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,59 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+//Importamos los componentes
+import {LoginComponent} from './components/login/login.component';
+import {RegistroComponent} from './components/registro/registro.component';
+import {HomeComponent} from './components/home/home.component';
+import {SugerenciasComponent} from './components/sugerencias/sugerencias.component';
+import {ContactosComponent} from './components/contactos/contactos.component';
+import {DescartesComponent} from './components/descartes/descartes.component';
+import {MatchsComponent} from './components/matchs/matchs.component';
+
+
+const routes: Routes = [
+{
+  path: '',
+  redirectTo: '/login',
+  pathMatch: 'full' 
+},
+
+{
+  path: 'login',
+  component: LoginComponent
+},
+
+{
+  path: 'registro',
+  component:RegistroComponent
+},
+ 
+{path: 'home',
+  component:HomeComponent},
+
+{
+  path: 'sugerencias',
+  component:SugerenciasComponent
+},
+
+{
+  path: 'contactos',
+  component: ContactosComponent
+},
+
+{
+  path:'descartes',
+  component: DescartesComponent
+},
+
+{
+  path: 'matchs',
+  component: MatchsComponent
+}
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/descartes/descartes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descartes',
+  templateUrl: './descartes.component.html',
+  styleUrls: ['./descartes.component.css']
+})
+export class DescartesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
