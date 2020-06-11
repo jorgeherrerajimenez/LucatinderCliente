265f5467554b22de6fada0ea67df916224cadee9
Marco Rueda <marcorueda92z@gmail.com>
2020-06-11 17:46:15 +0200

    commit para descargar cambios

---
+++ src/app/model/perfil.ts
@@ -0,0 +1,19 @@
+export class Perfil {
+
+    //creamos los atributos de la clase
+    public id:Number;
+    public nombre:String;
+    public genero:String;
+    public edad:Number;
+    public descripcion:String;
+
+    constructor(id: Number, nombre: string, genero: string, edad: Number, descripcion: string){
+
+        this.id = id;
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.descripcion = descripcion;
+    }
+
+}
