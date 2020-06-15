8b2ff6c1a9626a41eb71c23ddfefba5e0c34b27d
Adnan H <adnansuara@gmail.com>
2020-06-15 09:56:05 +0200

    Rama Adnan

--- src/app/components/sugerencias/sugerencias.component.ts
+++ src/app/components/sugerencias/sugerencias.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Perfil } from 'src/app/model/perfil';
 import { PerfilService } from 'src/app/services/perfil.service';
-import { HttpClientModule} from "@angular/common/http";
 //Nuevo
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-sugerencias',
   templateUrl: './sugerencias.component.html',
-  styleUrls: ['./sugerencias.component.css']
+  styleUrls: ['./sugerencias.component.css'],
+  providers: [PerfilService]
 })
+
+
+
 export class SugerenciasComponent implements OnInit {
   
   sugerencias:Perfil[];
@@ -21,10 +25,10 @@ export class SugerenciasComponent implements OnInit {
   
   
   ngOnInit(): void {
-    this.perfilService.getSugerencias(this.perfil)
-
+    this.perfilService.getSugerencias(5)
       .subscribe(params => data => {
         this.sugerencias = data;
+        console.log(this.sugerencias);
       });
   };
 
