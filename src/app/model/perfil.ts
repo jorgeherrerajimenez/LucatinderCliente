ead0c382bcc16cb69ef8f788794ceee49046f2bd
Maira Peña Quiroz <maira.pquiroz@gmail.com>
2020-06-18 09:46:24 +0200

    Actualización Rama Maira

--- src/app/model/perfil.ts
+++ src/app/model/perfil.ts
@@ -12,14 +12,5 @@ export class Perfil {
     public descripcion:String;
     public image:String;
   
-  
-    constructor(id: Number, nombre: string, genero: string, edad: Number, descripcion: string, image:String){
-        this.id = id;
-        this.nombre = nombre;
-        this.genero = genero;
-        this.edad = edad;
-        this.descripcion = descripcion;
-        this.image = image;
-    }
 
 }
